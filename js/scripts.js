8e7dfd7834f50afdfa789efcfe3fed895083d3ff
mark-oblack <moblack42@gmail.com>
2018-01-08 19:07:08 -0500

    added pseudo code to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Pseudo Code
+//
+//1) User is prompted to allow browser to view location
+//2) App uses user's location to retrieve weather from API
+//3) App displays the weather for the user's location
+//4) Image displays reflecting current weather conditions
+//5) Button can be clicked to toggle between farenheit and celsius
+//	a) Display farenheit by default, store in variable
+//	b) When user clicks button, farenheit is calculated into Celsius
+//	c) When button is clicked again, variable containing farenheit value is displayed
+//
