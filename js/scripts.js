bda9012c2c52fe6803c2c239a639affb88ce9d56
mark-oblack <moblack42@gmail.com>
2018-02-13 19:05:19 -0500

    pulled in data for celsius and farenheit temp

--- js/scripts.js
+++ js/scripts.js
@@ -10,18 +10,29 @@
 //	c) When button is clicked again, variable containing farenheit value is displayed
 //
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    //$(".location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-	var api="https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "&lon=" + longitude;
-		$.getJSON(api, function(data){
-			var weatherType = data.weather[0].description;
-			console.log(weatherType);
+$(document).ready(function() {
+	var latitude;
+	var longitude;
+	var fareneheit;
+	var celsius;
+	var city;
+	var weatherType;
+	if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(function(position) {
+	    //$(".location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+	    latitude = position.coords.latitude;
+	    longitude = position.coords.longitude;
+		var api="https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "&lon=" + longitude;
+			$.getJSON(api, function(data){
+				weatherType = data.weather[0].description;
+				city = data.name;
+				celsius = data.main.temp;
+				farenheit = ((celsius * 1.8) + 32);
+				console.log(farenheit);
+			});
 		});
-	});
-}
+	}
+});
 
 /*$(document).ready(function(){
 	var api="api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}";
